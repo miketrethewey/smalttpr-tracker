507e9467e372f2186e0d8648c35dbb369b4b3035
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-07-21 20:39:14 -0700

    Document code

--- script/shared.js
+++ script/shared.js
@@ -1,13 +1,17 @@
+// Add ucfirst() to String pseudoclass
 String.prototype.ucfirst = function () {
-    return this.substr(0,1).toUpperCase() + this.slice(1);
+    return this.substring(0,1).toUpperCase() + this.slice(1);
 }
 
-const CRYSTAL = 0;
-const OJCRYSTAL = 1;
-const OFFPENDANT = 2;
-const GREENPENDANT = 3;
-const Z1FACTOR = 4;
+// FIXME: Probably deprecated
+const CRYSTAL       = 0;    // Z3 Blue      Crystal Dungeon Prize
+const OJCRYSTAL     = 1;    // Z3 Red       Crystal Dungeon Prize
+const OFFPENDANT    = 2;    // Z3 Red/Blue  Pendant Dungeon Prize
+const GREENPENDANT  = 3;    // Z3 Green     Pendant Dungeon Prize
 
+const Z1FACTOR      = 4;    // Z1 Map Zoom Factor
+
+// From a URL, with a key name, get the value, if it exists, else return defaultVal
 function getParameterByName(name, url, defaultVal) {
     if (!url) url = window.location.href;
     if (!defaultVal && defaultVal !== null) defaultVal = "";
@@ -19,6 +23,7 @@ function getParameterByName(name, url, defaultVal) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+// Extend an object
 function extend(obj, src) {
     for (var key in src) {
         if (src.hasOwnProperty(key)) obj[key] = src[key];
@@ -26,24 +31,36 @@ function extend(obj, src) {
     return obj;
 }
 
-var manifests = [];
-var g = "";
-var bosses = 0;
+var manifests = []; // Hold collected game manifests
+var g = "";         // Reusable GameID
+var bosses = 0;     // Number of Bosses
 
+// Get selected game, default to zelda3
 var selectedGame = getParameterByName("game",window.location,"zelda3");
+
+// Post effective version of the included logic
 var effectiveVersion = "";
 
-var regionObjects = {};
-var regionNames = {};
+var regionObjects = {}; // Collect region objects to call in logic
+var regionNames = {};   // Collect region names
+
+// Z3 can use canned breadth logic or graph logic
 var zeldaMode = getParameterByName("zeldaMode",window.location,"oldstyle");
+
+// FIXME: I don't remember what this does
 var metroidMode = getParameterByName("metroidMode",window.location,"");
 
+// TODO: Setup for Z1 Quest ID
 var questid = getParameterByName("questid",window.location,1);
+
+// FIXME: Unused?
 var authAttempted = false;
 
 function destroyFirebase() {
+    // do nothing
 }
 
+// Make a nice standardized handle for css classes and object keys
 function tokenize(input) {
     let output = input;
     let replacements = {
@@ -58,6 +75,7 @@ function tokenize(input) {
     return output.toLowerCase();
 }
 
+// Massage token into reasonable title attribute value
 function fix_itemlabel(item) {
     var ret = item;
     if(itemNames[ret]) {
@@ -77,6 +95,7 @@ function fix_itemlabel(item) {
     return ret;
 }
 
+// Figure out best image to use
 function build_img_url(item,useGame = selectedGame) {
     var misc = [
         "blank",
@@ -84,97 +103,134 @@ function build_img_url(item,useGame = selectedGame) {
         "poi"
     ];
 
-    var zelda3items     = "zelda3"      in gameItems ? gameItems.zelda3 : [];
-    var zelda1items     = "zelda1"      in gameItems ? gameItems.zelda1 : [];
-    var metroid3items   = "metroid3"    in gameItems ? gameItems.metroid3 : [];
-    var metroid1items   = "metroid1"    in gameItems ? gameItems.metroid1 : [];
-    var averge1items    = "averge1"     in gameItems ? gameItems.averge1 : [];
+    // Most are PNGs
     let filext = "png";
 
-    let itemKey = item;
-    if(item.substring(1,2) == "1" || item.substring(1,2) == "3") {
-        item = item.substring(2);
-    }
-
     // Not Boss & not Chest
-    if((item.indexOf("boss") == -1) && (item.indexOf("chest") == -1)) {
-        if(item == "bomb") {
-            useGame = "zelda3";
-        } else if(metroid3items.indexOf(itemKey) > -1 || metroid3items.indexOf(itemKey.substr(0,itemKey.length-1)) > -1) {
-            useGame = "metroid3";
-        } else if(metroid1items.indexOf(itemKey) > -1 || metroid1items.indexOf(itemKey.substr(0,itemKey.length-1)) > -1) {
-            useGame = "metroid1";
-        } else if(zelda1items.indexOf(itemKey) > -1 || zelda1items.indexOf(itemKey.substr(0,itemKey.length-1)) > -1) {
-            useGame = "zelda1";
-        } else if(zelda3items.indexOf(itemKey) > -1 || zelda3items.indexOf(itemKey.substr(0,itemKey.length-1)) > -1) {
-            useGame = "zelda3";
-        } else if(averge1items.indexOf(itemKey) > -1 || averge1items.indexOf(itemKey.substr(0,itemKey.length-1)) > -1) {
-            useGame = "averge1";
+    let itemKey = item;
+    let itemKe  = Number.isInteger(parseInt(item.substr(-1))) ? item.substr(0,item.length-1) : item;
+    if(
+        (item.indexOf("boss") == -1) &&
+        (item.indexOf("chest") == -1)
+    ) {
+        // Check other games
+        // FIXME: Get this from Master Manifest
+        for(let gameCheck of [
+            "metroid3",
+            "metroid1",
+            "zelda1",
+            "zelda3",
+            "averge1"
+        ]) {
+            if(
+                gameItems[gameCheck] &&
+                (
+                    (gameItems[gameCheck].indexOf(itemKey) > -1) ||
+                    (gameItems[gameCheck].indexOf(itemKe) > -1)
+                )
+            ) {
+                useGame = gameCheck;
+            }
         }
     }
-    if(selectedGame == "zelda1" && item.indexOf("boss5") > -1) {
+    // Z1Gohma is animated because why not
+    if(item == "z1boss5") {
         filext = "gif";
     }
 
-    var globalReplaceItem = {
-        agahnim:    "agahnim1",
-        bomb:       "bomb1",
-        bomb0:      "bomb1",
-        boomerang0: "boomerang1",
-        bottle:     "bottle1",
-        bottle0:    "bottle1",
-        candle:     "candle1",
-        candle0:    "candle1",
-        crystal5:   "dungeon" + OJCRYSTAL,
-        crystal6:   "dungeon" + OJCRYSTAL,
-        flute:      "flute0",
-        glove0:     "glove1",
-        lamp:       "lantern",
-        medallion1: "bombos",
-        medallion2: "ether",
-        medallion3: "quake",
-        pendant0:   "dungeon" + GREENPENDANT,
-        ring0:      "ring1",
-        shield0:    "shield1",
-        sword0:     "sword1",
-    };
-    globalReplaceItem["blueCrystal"] = "dungeon" + CRYSTAL;
-    globalReplaceItem["redCrystal"] = "dungeon" + OJCRYSTAL;
+    // Shave off the prefix
+    let prefix = manifests[useGame]["prefix"];
+    if(item.startsWith(prefix)) {
+        item = item.replace(manifests[useGame]["prefix"],"");
+    }
 
+    var globalReplaceItem = {
+        agahnim:    "agahnim1",     // Z3 Lumberjack Tree: Mini
+        bomb:       "bomb1",        // Z3Bombs: Mini
+        bomb0:      "bomb1",        // Z3Bombs: Off
+        boomerang0: "boomerang1",   // Z3Boomerang: Off
+        bottle:     "bottle1",      // Z3Bottle: Mini
+        flute:      "flute0",       // Z3 Weathervane
+        glove0:     "glove1",       // Z3Glove: Off
+        // lamp:       "lantern",   // FIXME: Maybe unused?
+        medallion1: "bombos",       // Z3Bombos: Mini
+        medallion2: "ether",        // Z3Ether: Mini
+        medallion3: "quake",        // Z3Quake: Mini
+        shield0:    "shield1",      // Z3Shield: Off
+        sword0:     "sword1",       // Z3Sword: Off
+        pendant0:   "dungeon" + GREENPENDANT,   // Z3 Saha
+
+        bottle0:    "bottle1",      // Z1Bottle: Off
+        candle:     "candle1",      // Z1Candle: Mini
+        candle0:    "candle1",      // Z1Candle: Off
+        ring0:      "ring1",        // Z1Ring: Off
+    };
+    // Blue Crystal
+    globalReplaceItem["blueCrystal"]    = "dungeon" + CRYSTAL;
+    // Red Crystals
+    globalReplaceItem["redCrystal"]     = "dungeon" + OJCRYSTAL;
+    globalReplaceItem["crystal5"]       = "dungeon" + OJCRYSTAL;
+    globalReplaceItem["crystal6"]       = "dungeon" + OJCRYSTAL;
+
+    // See if we've got a replacement
     if(globalReplaceItem[item]) {
         item = globalReplaceItem[item];
     }
 
+    // Default to inventory
     var category = "inventory";
+    // These categories are in these folders
     for([start,cat] of Object.entries({
-        "boss": "bosses",
-        "chest": "chests",
-        "medallion": "medallions",
-        "dungeon": "prizes",
-        "pendant": "prizes"
+        "boss":         "bosses",
+        "chest":        "chests",
+        "medallion":    "medallions",
+        "dungeon":      "prizes",
+        "pendant":      "prizes"
     })) {
         if(item.startsWith(start)) {
             category = cat;
         }
     }
+
+    // Misc category
     if(misc.indexOf(item) > -1) {
         category = "misc";
     }
 
+    // Start URL
     var url = "images/";
+    // Not Misc has a game theme
     if(category != "misc") {
         url += useGame + '/';
     }
+    // Build the rest
     url += category + '/' + item + '.' + filext;
 
+    // Print it & ship it!
     return url;
 }
 
 function mini(item) {
-    if(item.substring(1,2) == "1" || item.substring(1,2) == "3") {
-        item = item.substring(2);
+    // Add the prefix
+    let itemKey = item;
+    let prefix = manifests[selectedGame]["prefix"];
+    if(!itemKey.startsWith(prefix)) {
+        itemKey = prefix + item;
     }
+
+    // Make basic title
     var title = item.ucfirst();
+
+    // Try to get title from manifest
+    // FIXME: Doesn't work if it's a progressive item
+    if(
+        manifests[selectedGame] &&
+        manifests[selectedGame]["items"] &&
+        manifests[selectedGame]["items"][itemKey] &&
+        manifests[selectedGame]["items"][itemKey]["name"]
+    ) {
+        title = manifests[selectedGame]["items"][itemKey]["name"];
+    }
 
     var globalReplaceTitle = {
         pendant1: "Pendant of Power",
@@ -184,8 +240,8 @@ function mini(item) {
     globalReplaceTitle["dungeon" + CRYSTAL]         = "Blue Crystal";
     globalReplaceTitle["dungeon" + OJCRYSTAL]       = "Red Crystal";
 
-    if(globalReplaceTitle[item]) {
-        title = globalReplaceTitle[item].ucfirst();
+    if(globalReplaceTitle[itemKey]) {
+        title = globalReplaceTitle[itemKey].ucfirst();
     }
 
     for(var i = 0; i < 10; i++) {
